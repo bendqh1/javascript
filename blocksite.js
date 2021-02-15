23df6046bc3ee0deefb0cdb9f60746cfe28f0cb4
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-15 22:16:40 +0700

    Create blocksite.js

---
+++ blocksite.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         blocksite
+
+// @match        *://example-one.com/*
+// @match        *://example-two.com/*
+
+// ==/UserScript==
+// Ranned with Tampermonkey
+
+(function() {
+    'use strict';
+    window.open("https://block-sxribe.glitch.me/", "_self");
+})();
