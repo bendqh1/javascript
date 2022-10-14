463a277bad725e77656201488a25b30ce684383c
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2022-10-14 14:02:22 +0300

    Update contact_box_with_text_whatsapp.js

--- contact_boxes/contact_box_with_text_whatsapp.js
+++ contact_boxes/contact_box_with_text_whatsapp.js
@@ -2,7 +2,7 @@ document.body.insertAdjacentHTML('beforeend', `
 	<aside class="cbwtphone_wrapper">
 		<a class="cbwtphone_link" href="https://wa.me:NUMBER">
 			<span class="cbwtphone_text">שיחת וואטסאפ</span>
-			<img class="cbwtphone_icon" src="https://en.wikipedia.org/wiki/WhatsApp#/media/File:WhatsApp.svg"></img>
+			<img class="cbwtphone_icon" src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"></img>
 		</a>
 	</aside>
 `)
