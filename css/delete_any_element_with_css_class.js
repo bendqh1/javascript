88b4c478fee8b146c01328c4acf70727563d517b
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-02-21 13:49:00 +0700

    Create delete_any_element_with_css_class.js

---
+++ css/delete_any_element_with_css_class.js
@@ -0,0 +1,8 @@
+window.setTimeout(()=>{
+    document.querySelectorAll([
+        "#n-Special-pages",
+        "#n-Webpages-to-create"
+    ]).forEach((element) => {
+        element.style.display = "none";
+    });
+}, 1000);
