3d013213b616600b8564faa9ba7a52a760716e7d
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-05-01 02:29:51 +0300

    Update stop_automatic_video_play.js

--- usm/stop_automatic_video_play.js
+++ usm/stop_automatic_video_play.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', () => {
   document.querySelectorAll('video').forEach(anyVideo => {
-    anyVideo.pause() 
+    anyVideo.pause();
   });
 });
 
 window.setTimeout( () => {
   document.querySelectorAll('video').forEach(anyVideo => {
+      anyVideo.pause();
+  });
 }, 2500);
