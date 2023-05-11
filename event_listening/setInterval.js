717582b94fdd097a14f13077560eed189d077e0d
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-05-11 03:23:56 +0300

    Update setInterval.js

--- event_listening/setInterval.js
+++ event_listening/setInterval.js
@@ -1,8 +1,3 @@
 window.setInterval( () => {
   // Code
 }, 5000);
-
-const myInterval = setInterval( () => {
-  // Code
-}, 2500);
-// We put setInterval in a variable in case we would want to clear the internval after some amount of executions;
