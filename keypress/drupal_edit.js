a621d81ed14852d9fe1420f36af38e18eff095eb
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-06-08 19:02:05 +0300

    Update drupal_edit.js

--- keypress/drupal_edit.js
+++ keypress/drupal_edit.js
@@ -1,5 +1,7 @@
 window.addEventListener('keydown', function(event) {
-  if (event.altKey && event.shiftKey && event.key === 'e') {
+  if (event.altKey && event.shiftKey && event.key === 'E') {
     document.querySelector('#block-olivero-primary-local-tasks > div.block__content > nav > ul > li:nth-child(2) > a').addEventListener("click", openEditPage).click();
   }
 });
+
+// event.key === 'e' didn't work event.key === 'E' did work;
