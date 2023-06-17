4e5aaee5c314ba2c611c563a555de879c13ef14b
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-06-17 19:04:49 +0300

    Delete delete_any_element_with_textContent.js

--- textContent/delete_any_element_with_textContent.js
+++
@@ -1,6 +0,0 @@
-const [...elements] = document.getElementsByTagName("*");
-elements.forEach((element) => {
-    if (element.textContent == "someTextContent") {
-        element.style.display = 'none';
-    }
-});
