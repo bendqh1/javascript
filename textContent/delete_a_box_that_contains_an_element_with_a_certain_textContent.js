8b2296c5a1d6c20efa26e9433f01778e4d107a54
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-06-17 19:05:04 +0300

    Delete delete_a_box_that_contains_an_element_with_a_certain_textContent.js

--- textContent/delete_a_box_that_contains_an_element_with_a_certain_textContent.js
+++
@@ -1,7 +0,0 @@
-const category_box = document.querySelector("#catlinks-sidebar");
-const [...atLeastOneElement] = document.getElementsByTagName("*");
-atLeastOneElement.forEach((element) => {
-    if (element.textContent == "קטגוריות מוסתרות") {
-        category_box.style.display = 'none';
-    }
-});
