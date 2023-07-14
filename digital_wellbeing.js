68d8a9b1c0d9fdba518e07cd3f621be5b5f50492
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-07-14 22:44:41 +0300

    Update digital_wellbeing.js

--- digital_wellbeing.js
+++ digital_wellbeing.js
@@ -1,4 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////
+// Block specific website/s
+////////////////////////////////////////////////////////////////////////////
+
+window.setInterval( () => {
+    const urlPatternToBlock = [
+        'https://search.google.com/search-console'
+    ];
+
+    for (const element of urlPatternToBlock) {
+        if (window.location.href.includes(urlPatternToBlock)) {
+            window.open("https://google.com/", "_self");
+        }
+    }
+}, 1);
+
+////////////////////////////////////////////////////////////////////////////
 // Block news websites as much as possible
 ////////////////////////////////////////////////////////////////////////////
 
@@ -36,20 +52,4 @@ window.setInterval( () => {
   document.querySelectorAll('video').forEach(anyVideo => {
     anyVideo.pause();
   });
-}, 1);
-
-////////////////////////////////////////////////////////////////////////////
-// Block specific website/s
-////////////////////////////////////////////////////////////////////////////
-
-window.setInterval( () => {
-    const urlPatternToBlock = [
-        'https://search.google.com/search-console'
-    ];
-
-    for (const element of urlPatternToBlock) {
-        if (window.location.href.includes(urlPatternToBlock)) {
-            window.open("https://google.com/", "_self");
-        }
-    }
 }, 1);
