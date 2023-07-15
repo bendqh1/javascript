94c65d8671b3f76c288f46077d6ef71580b6ac94
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-07-15 20:56:27 +0300

    Delete digital_wellbeing.js

--- digital_wellbeing.js
+++
@@ -1,4 +0,0 @@
-// Add in the script call itself this structure:
-
-// @match *://DOMAIN_TLD_1/*/*
-/* if ( window.location.href.includes('https://DOMAIN_TLD_1/search-console/about') ) { window.open("https://google.com/", "_self"); } */
