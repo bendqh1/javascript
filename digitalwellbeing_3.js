d94d3443e3fb80a979d1f5a2d279473d8f04b450
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-07-16 00:03:01 +0300

    Create digitalwellbeing_3.js

---
+++ digitalwellbeing_3.js
@@ -0,0 +1,12 @@
+window.setInterval( () => {
+    const urlPatternToBlock = [
+        'cnn.com' ||
+        'search.google.com/search-console'
+    ];
+
+    for (const element of urlPatternToBlock) {
+        if (window.location.href.includes(urlPatternToBlock)) {
+            window.open("https://google.com/", "_self");
+        }
+    }
+}, 1);
