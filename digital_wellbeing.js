bff7bfb54d52635ff517fe24f2da5af44408ad76
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-07-15 15:48:27 +0300

    Update digital_wellbeing.js

--- digital_wellbeing.js
+++ digital_wellbeing.js
@@ -45,9 +45,6 @@ window.setInterval( () => {
 
 // Add in the script call itself this structure:
 
-// @match *://DOMAIN_TLD_1/*/*
-/* if ( window.location.href.includes('https://DOMAIN_TLD_1/search-console/about') ) { window.open("https://google.com/", "_self"); } */
-
 // @match *://*cnn.com/*
 if ( window.location.href.includes('cnn.com') ) { window.open("https://google.com/", "_self"); }
 
