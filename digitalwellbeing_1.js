8c42bfc7de1a098acb6005f216ff018ca8368cbc
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-07-15 22:37:26 +0300

    Update digitalwellbeing_1.js

--- digitalwellbeing_1.js
+++ digitalwellbeing_1.js
@@ -1,11 +1,13 @@
 if (
-  /* document.querySelector('title').textContent.includes('Breaking News') || */
+  document.querySelector('title')?.textContent.includes('News') ||
+  document.querySelector('title')?.textContent.includes('news') ||
   document.querySelector('meta[name="title"]')?.content.includes('News') ||
   document.querySelector('meta[name="title"]')?.content.includes('news') ||
   document.querySelector('meta[name="description"]')?.content.includes('News') ||
   document.querySelector('meta[name="description"]')?.content.includes('news') ||
   document.querySelector('meta[name="tags"]')?.content.includes('News') ||
   document.querySelector('meta[name="tags"]')?.content.includes('news') ||
+  /**/
   document.querySelector('title')?.textContent.includes('חדשות') ||
   document.querySelector('title')?.textContent.includes('החדשות') ||
   document.querySelector('meta[name="title"]')?.content.includes('חדשות') ||
