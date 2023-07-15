699b6d0685d85b648cb70d53b5405f5eecd512ef
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-07-15 20:52:46 +0300

    Update digital_wellbeing.js

--- digital_wellbeing.js
+++ digital_wellbeing.js
@@ -1,42 +1,9 @@
-////////////////////////////////////////////////////////////////////////////
-// Block news websites as much as possible
-////////////////////////////////////////////////////////////////////////////
-
-if (
-  /* document.querySelector('title').textContent.includes('Breaking News') || */
-  document.querySelector('meta[name="title"]')?.content.includes('News') ||
-  document.querySelector('meta[name="title"]')?.content.includes('news') ||
-  document.querySelector('meta[name="description"]')?.content.includes('News') ||
-  document.querySelector('meta[name="description"]')?.content.includes('news') ||
-  document.querySelector('meta[name="tags"]')?.content.includes('News') ||
-  document.querySelector('meta[name="tags"]')?.content.includes('news') ||
-  document.querySelector('title')?.textContent.includes('חדשות') ||
-  document.querySelector('title')?.textContent.includes('החדשות') ||
-  document.querySelector('meta[name="title"]')?.content.includes('חדשות') ||
-  document.querySelector('meta[name="title"]')?.content.includes('החדשות') ||
-  document.querySelector('meta[name="description"]')?.content.includes('חדשות') ||
-  document.querySelector('meta[name="description"]')?.content.includes('החדשות') ||
-  document.querySelector('meta[name="tags"]')?.content.includes('חדשות') ||
-  document.querySelector('meta[name="tags"]')?.content.includes('החדשות')
-) {
-  window.open("https://google.com/", "_self");
-}
-
-/*
-  window?.location.href.includes('news') ||
-  window?.location.href.includes('News') ||
-  window?.location.href.includes('חדשות') ||
-  window?.location.href.includes('החדשות') ||
-*/
-
-////////////////////////////////////////////////////////////////////////////
-// Block videos autoplay
-////////////////////////////////////////////////////////////////////////////
-
 window.setInterval( () => {
-  document.querySelectorAll('video').forEach(video => {
-    video.pause();
-  });
+  if ( document.querySelectorAll('video') ) {
+    document.querySelectorAll('video').forEach(video => {
+      video.pause();
+    });
+  };
 }, 1);
 
 ////////////////////////////////////////////////////////////////////////////
