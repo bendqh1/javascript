29ce50a23215a956e1f67b66cf28820eff75606a
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-12-04 02:55:30 +0200

    Update scroll_to_the_middle_of_a_webpage.js

--- scrolling/scroll_to_the_middle_of_a_webpage.js
+++ scrolling/scroll_to_the_middle_of_a_webpage.js
@@ -1,17 +1,18 @@
-const myInterval = setInterval( () => {
-    // go downmost in a webpage for 1 second to discover its height
+// go to downmost part of a webpage to discover its height
+window.setTimeout(()=>{
     window.scrollTo(0, document.body.scrollHeight);
-}, 0.);
-window.setTimeout( () => {
-    clearInterval(myInterval);
-}, 2500);
-  
-// go back to topmost part of the discovered webpage
-window.scrollTo({
-    top: 100,
-    left: 100,
-    behavior: "instant",
-});
+}, 1000)
 
-// scroll to the middle of a discovered webpage
-window.scrollTo(0, document.documentElement.scrollHeight * 0.50);
+window.setTimeout(()=>{
+// go back to topmost part of the discovered webpage
+    window.scrollTo({
+        top: 100,
+        left: 100,
+        behavior: "instant",
+    });
+}, 2000)
+
+window.setTimeout(()=>{
+    // scroll to the middle of a discovered webpage
+    window.scrollTo(0, document.documentElement.scrollHeight * 0.50);
+}, 3000)
