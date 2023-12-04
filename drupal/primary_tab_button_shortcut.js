fd66a45fe76aa7c54251ac870402b376d9bc0dd3
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2023-12-04 13:52:56 +0200

    Update and rename primary_tab_button_shortcut.js to primary_tab_button_shortcut.md

--- drupal/primary_tab_button_shortcut.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('keydown', function(event) {
-  if (event.altKey && event.shiftKey && event.key === 'E') {
-    document.querySelector('#block-olivero-primary-local-tasks > div.block__content > nav > ul > li:nth-child(2) > a').click();
-  }
-});
-
-// event.key === 'e' didn't work event.key === 'E' did work;
