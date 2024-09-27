059c6b35e9497b2201cce7c88a3305eec04fa9a6
Ben Aharoni <105626068+bendqh1@users.noreply.github.com>
2024-09-27 15:03:13 +0700

    Update modules.js

--- contact_boxes/modules.js
+++ contact_boxes/modules.js
@@ -1,1 +1,10 @@
+document.body.insertAdjacentHTML('beforeend', `
+    // HTML COMES HERE
+`)
 
+newStyle = document.createElement("style");
+newStyle.type = "text/css";
+newStyle.innerHTML +=`
+    // CSS COMES HERE
+`;
+document.head.appendChild(newStyle);
